5ffd7a76e038a4c8edd777bce34a6533cc677938
Newton Munene <newtonmunene99@gmail.com>
2019-07-27 18:48:02 +0300

    Update audio.dart

--- lib/services/audio.dart
+++ lib/services/audio.dart
@@ -6,23 +6,29 @@ import 'package:logging/logging.dart';
 import 'package:palette_generator/palette_generator.dart';
 import 'package:rxdart/rxdart.dart';
 
+enum RepeatState { ON, OFF, ONE }
+enum ShuffleState { ON, OFF }
+
 class AudioService {
-  FlutterAudioQuery audioQuery;
+  FlutterAudioQuery _audioQuery;
   AudioPlayer audioPlayer;
-  List<SongInfo> nowplayinglist;
+  RepeatState repeatState = RepeatState.OFF;
+  ShuffleState shuffleState = ShuffleState.OFF;
   final Logger log = new Logger('AudioService');
 
   BehaviorSubject<PaletteGenerator> _colorPaletteController;
   BehaviorSubject<SongInfo> _currentlyPlayingController;
   BehaviorSubject<List<SongInfo>> _songsController;
+  BehaviorSubject<List<SongInfo>> _nowPlayingSongsController;
   BehaviorSubject<List<ArtistInfo>> _artistsController;
   BehaviorSubject<List<AlbumInfo>> _albumsController;
 
   AudioService() {
-    audioQuery = FlutterAudioQuery();
+    _audioQuery = FlutterAudioQuery();
     audioPlayer = AudioPlayer();
-    nowplayinglist = [];
+
     _songsController = BehaviorSubject<List<SongInfo>>();
+    _nowPlayingSongsController = BehaviorSubject<List<SongInfo>>();
     _currentlyPlayingController = BehaviorSubject<SongInfo>();
     _colorPaletteController = BehaviorSubject<PaletteGenerator>();
     _artistsController = BehaviorSubject<List<ArtistInfo>>();
@@ -32,28 +38,37 @@ class AudioService {
   Stream<PaletteGenerator> get palette => _colorPaletteController.stream;
   Stream<SongInfo> get currentyPlaying => _currentlyPlayingController.stream;
   Stream<List<SongInfo>> get songs => _songsController.stream;
+  Stream<List<SongInfo>> get nowplayingsongs =>
+      _nowPlayingSongsController.stream;
+  List<SongInfo> get getnowplayingsongs => _nowPlayingSongsController.value;
   Stream<List<ArtistInfo>> get artists => _artistsController.stream;
   Stream<List<AlbumInfo>> get albums => _albumsController.stream;
 
   getMusicFiles() async {
-    var songs = await audioQuery.getSongs();
-    _songsController.add(songs);
+    var tracks = await _audioQuery.getSongs();
+    _songsController.add(tracks);
 
-    var albums = await audioQuery.getAlbums();
+    this.songs.listen((songslist) {
+      this._nowPlayingSongsController.add(songslist);
+    });
+
+    var albums = await _audioQuery.getAlbums();
     _albumsController.add(albums);
 
-    var artists = await audioQuery.getArtists();
+    var artists = await _audioQuery.getArtists();
     _artistsController.add(artists);
 
-    audioPlayer.onPlayerStateChanged.listen((state) {
-      if (state == AudioPlayerState.COMPLETED) {
+    audioPlayer.onPlayerCompletion.listen((completed) {
+      if (repeatState == RepeatState.ONE) {
+        play(_currentlyPlayingController.value);
+      } else {
         next();
       }
     });
 
-    audioPlayer.onAudioPositionChanged.listen((onData) {
-      log.fine(onData.inSeconds);
-    });
+    // audioPlayer.onAudioPositionChanged.listen((onData) {
+    //   log.fine(onData.inSeconds);
+    // });
   }
 
   Future<void> play(SongInfo song) async {
@@ -61,12 +76,44 @@ class AudioService {
       await stop();
       _currentlyPlayingController.add(song);
       _getProminentColor();
-      await audioPlayer.play(song.filePath, isLocal: true);
+      await audioPlayer.play(
+        song.filePath,
+        isLocal: true,
+        respectSilence: false,
+      );
     } else {
       _currentlyPlayingController.add(song);
       _getProminentColor();
-      await audioPlayer.play(song.filePath, isLocal: true);
+      await audioPlayer.play(
+        song.filePath,
+        isLocal: true,
+        respectSilence: false,
+      );
     }
+  }
+
+  void shuffle() {
+    var list = List<SongInfo>.from(_nowPlayingSongsController.value);
+    list.shuffle();
+    _nowPlayingSongsController.add(list);
+  }
+
+  void unshuffle() {
+    var list = List<SongInfo>.from(_songsController.value);
+    log.fine(list[0].title);
+    _nowPlayingSongsController.add(list);
+    log.fine(list[0].title);
+  }
+
+  Future<void> reorder(int previous, int current) async {
+    log.fine('$previous, $current');
+    var list = _nowPlayingSongsController.value;
+    var song = list.removeAt(previous);
+    return list.insert(current, song);
+    // log.fine("${list[current].title}");
+    // log.fine("${list[previous].title}");
+
+    // _nowPlayingSongsController.add(list);
   }
 
   void _getProminentColor() async {
@@ -84,6 +131,10 @@ class AudioService {
     }
   }
 
+  Future<void> resume() async {
+    await audioPlayer.resume();
+  }
+
   Future<void> pause() async {
     await audioPlayer.pause();
   }
@@ -96,7 +147,7 @@ class AudioService {
   Future<void> next() async {
     var current = _currentlyPlayingController.value;
 
-    var songslist = _songsController.value;
+    var songslist = _nowPlayingSongsController.value;
     int nextIndex = songslist.indexOf(current) + 1;
 
     if (nextIndex < songslist.length) {
@@ -107,7 +158,7 @@ class AudioService {
   Future<void> previous() async {
     var current = _currentlyPlayingController.value;
 
-    var songslist = _songsController.value;
+    var songslist = _nowPlayingSongsController.value;
     int previousIndex = songslist.indexOf(current) - 1;
     if (previousIndex > -1) {
       await play(songslist[previousIndex]);
@@ -116,6 +167,7 @@ class AudioService {
 
   dispose() {
     _songsController.close();
+    _nowPlayingSongsController.close();
     _albumsController.close();
     _artistsController.close();
     _currentlyPlayingController.close();
